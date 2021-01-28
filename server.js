237a20dc83c8d263f3bfb44a5417ed0009caaf18
Josh Allan <allan.josh07@gmail.com>
2021-01-28 15:29:19 -0500

    Video 19 complete: created a connection to the database and created the logic for handling unhandled rejections.

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
-
-// route files
-const bootcamps = require('./routes/bootcamps');
+// bring in the database connection method
+const connectDB = require('./config/db');
 
 // load the config file
 dotenv.config({ path: './config/config.env' });
 
+// route files
+const bootcamps = require('./routes/bootcamps');
+
+// connect to the database
+connectDB();
+
+// create the app variable from express
 const app = express();
 
 // dev logging middleware
@@ -20,8 +26,15 @@ app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 4040;
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(
     `Server running in a ${process.env.NODE_ENV} environment, and listening on port http://localhost:${PORT}.`
   );
+});
+
+// handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  // close the server connection and exit process with failure flag (1)
+  server.close(() => process.exit(1));
 });
