a12a9997d14343fe200b1ff2cea4e6e11cec87d2
Josh Allan <allan.josh07@gmail.com>
2021-01-28 10:49:12 -0500

    Initial express setup (video 12).

--- server.js
+++ server.js
@@ -1,53 +1,15 @@
-const http = require('http');
+const express = require('express');
+const dotenv = require('dotenv');
 
-const todos = [
-  { id: 1, text: 'Todo 1' },
-  { id: 2, text: 'Todo 2' },
-  { id: 3, text: 'Todo 3' }
-];
+// load the config file
+dotenv.config({ path: './config/config.env' });
 
-const server = http.createServer((req, res) => {
-  const { method, url } = req;
-  let body = [];
+const app = express();
 
-  req
-    .on('data', (chunk) => {
-      body.push(chunk);
-    })
-    .on('end', () => {
-      body = Buffer.concat(body).toString();
+const PORT = process.env.PORT || 4040;
 
-      let status = 404;
-      const response = { success: false, data: null };
-
-      if (method === 'GET' && url === '/todos') {
-        status = 200;
-        response.success = true;
-        response.data = todos;
-      } else if (method === 'POST' && url === '/todos') {
-        const { id, text } = JSON.parse(body);
-
-        if (!id || !text) {
-          status = 400;
-        } else {
-          todos.push({ id, text });
-          status = 201;
-          response.success = true;
-          response.data = todos;
-        }
-      }
-
-      res.writeHead(status, {
-        'Content-Type': 'application/json',
-        'X-Powered-By': 'Node.js'
-      });
-
-      res.end(JSON.stringify(response));
-    });
+app.listen(PORT, () => {
+  console.log(
+    `Server running in a ${process.env.NODE_ENV} environment, and listening on port http://localhost:${PORT}.`
+  );
 });
-
-const PORT = 7070;
-
-server.listen(PORT, () =>
-  console.log(`Listening on port http://localhost:${PORT}`)
-);
